7815c09c5e2cb5d29745e3e74c520bdaa3b13e0f
Mauro.V <remanomauro@gmail.com>
2023-01-31 10:42:46 +0100

    add generatedStaticParams

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -1,12 +1,21 @@
 import Image from "next/image"
 
+export async function generateStaticParams(){
+  const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
+  const res = await data.json()
+  return res.results.map((movie) => ({
+    movie: toString(movie.id),
+  }))
+}
+
 export default async function MovieDetail({ params }){
   const { movie } = params
   console.log(params)
   const imagePath = "https://image.tmdb.org/t/p/original"
-  const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`, {next: {revalidate : 60 }})
-
+  const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`, {next: {revalidate : 4 }})
+  
   const res = await data.json()
+
   return(
     <div>
     <div>
@@ -26,7 +35,6 @@ export default async function MovieDetail({ params }){
           <div>
           <p>{res.overview}</p>
           </div>
-
     </div>
     </div>
   )
