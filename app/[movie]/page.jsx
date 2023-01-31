c9e2992bc35ff04ab2e44efb2afb4876a5158736
Mauro.V <remanomauro@gmail.com>
2023-01-31 10:13:10 +0100

    [movie] detail page final se up

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -1,7 +1,33 @@
-export default async function MovieDetail(){
+import Image from "next/image"
+
+export default async function MovieDetail({ params }){
+  const { movie } = params
+  console.log(params)
+  const imagePath = "https://image.tmdb.org/t/p/original"
+  const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`, {next: {revalidate : 60 }})
+
+  const res = await data.json()
   return(
     <div>
-    <h1>movies details</h1>
+    <div>
+      <h2 className="text-4xl">{res.title}</h2>
+      <h2 className="text-4xl">{res.release_date}</h2>
+      <h2 className="text-sm">Runtime: {res.runtime} minutes</h2>
+      <h2 className="bg-green-500 inline-block my-2 py-2 px-4 rounded">{res.status}</h2>
+          <Image className="my-12 w-full"
+          src={imagePath + res.backdrop_path}
+          alt="images"
+          width={800}
+          height={800}
+          priority
+          >
+          </Image>
+
+          <div>
+          <p>{res.overview}</p>
+          </div>
+
+    </div>
     </div>
   )
 }
