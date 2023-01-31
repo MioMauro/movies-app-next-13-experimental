2d20422f591cb4f61df5c05b0ef9766c8990fcac
Mauro.V <remanomauro@gmail.com>
2023-01-31 12:57:19 +0100

    fixed layout.js

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,28 +1,11 @@
 import './globals.css'
-import { Red_Hat_Text } from '@next/font/google'
-import Link from 'next/link'
-
-const redHat = Red_Hat_Text({
-  subsets: ['latin'],
-  weight: ['700', '600'],
-  variable: '--font-redHat',
-})
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head />
       <body className=' mx-32 my-12'>
-      <nav className='flex gap-9 mb-6 bg-gray-100 p-4 rounded'>
-      <Link href={`/`}>
-        <h1 className={`${redHat.className}`}><span>POPULAR FILM</span></h1>
-      </Link>
-        <ul>
-          <li className={`${redHat.className}`}>
-            <a href='/about'>About</a>
-          </li>
-        </ul>
-      </nav>
+      
       {children}
       </body>
     </html>
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,30 @@
 import Movie from "@/components/Movie"
+import Link from "next/link"
+import { Red_Hat_Text } from '@next/font/google'
+
+const redHat = Red_Hat_Text({
+  subsets: ['latin'],
+  weight: ['700', '600'],
+  variable: '--font-redHat',
+})
 
 export default async function Home(){
   const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
   const res = await data.json()
   console.log(res)
 
-  return(
+  return(<div>
+    <nav className='flex gap-9 mb-6 bg-gray-100 p-4 rounded'>
+      <Link href={`/`}>
+        <h1 className={`${redHat.className}`}><span>POPULAR FILM</span></h1>
+      </Link>
+        <ul>
+          <li className={`${redHat.className}`}>
+            <a href='/about'>About</a>
+          </li>
+        </ul>
+      </nav>
+      
     <div className="grid gap-16 grid-cols-fluid shadow-md p-2">
       {res.results.map((movie) => (        
         <Movie 
@@ -16,6 +35,7 @@ export default async function Home(){
           release_date={movie.release_date}
         />
       ))}      
+    </div>
     </div>
   )
 }
